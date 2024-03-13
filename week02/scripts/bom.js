94301b476d446b79d05a80d18929367e9a11e036
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-03-13 20:26:49 -0300

    Practice with DOM manipulation

---
+++ week02/scripts/bom.js
@@ -0,0 +1,38 @@
+
+
+
+const list = document.getElementById("list");
+
+const input = document.getElementById("favchap");
+
+const btn = document.querySelector("button");
+
+
+btn.addEventListener("click", () => {
+
+    if (input.value != "")
+    {
+            const liBookName = document.createElement("li");
+
+            const deleteBtn = document.createElement("button");
+
+            liBookName.textContent = input.value;
+            deleteBtn.textContent = "❌";
+
+            liBookName.append(deleteBtn);
+            list.append(liBookName);
+            
+
+            deleteBtn.addEventListener("click", () => {
+                list.removeChild(liBookName);
+                input.focus();
+                input.value = "";
+            });
+            
+        }
+    else {
+        alert("You need to enter a book and Chapter!")
+        input.focus();
+    };
+});
+
