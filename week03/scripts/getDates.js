6a659550372c3481a501172f35fedbb4aaf9e30a
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-03-19 18:18:28 -0300

    Practice with lazy load img in html and some css animation

---
+++ week03/scripts/getDates.js
@@ -0,0 +1,6 @@
+// copyright year
+document.querySelector('#year').textContent = new Date().getFullYear();
+
+
+document.querySelector("#lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
+
