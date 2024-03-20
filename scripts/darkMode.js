a0ad1db0f489e3cb8ac6285100fd8b953d9898e8
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-03-20 16:20:28 -0300

    add a google maps refference to the city where i live on the home page of the entire project

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -8,12 +8,14 @@ const section = document.querySelectorAll(".card");
 
 const  $main = document.querySelector("main");
 
+const $heroImgText = document.querySelector(".hero-label")
+
 $darkButton.addEventListener("click", () => {
     if ($darkButton.src.includes("images/moon.svg")) {
         
         $body.style.background = "#000";
         $main.style.color = "#fff";
-
+        $heroImgText.style.color = "#000";
         section[0].style.color = "#000";
         section[1].style.color = "#000";
 
