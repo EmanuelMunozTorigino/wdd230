911dc6f6b5e34f595913671e02594f52c430c1eb
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-04-09 22:09:49 -0300

    Form page

---
+++ scripts/formvalidation.js
@@ -0,0 +1,34 @@
+const password1 = document.querySelector("#password1");
+const password2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
+
+password2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (password1.value !== password2.value) {
+		message.textContent = "❗Passwords Do Not Match!, try again.";
+		message.style.visibility = "show";
+		password2.style.backgroundColor = "#fff0f3";
+		password2.value = "";
+        password1.value = "";
+		password1.focus();
+	} else {
+		message.style.display = "none";
+		password2.style.backgroundColor = "#fff";
+		password2.style.color = "#000";
+	}
+}
+
+
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("range");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = `Value: ${range.value}`;
+}
