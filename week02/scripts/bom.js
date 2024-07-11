8d3068018e749db33abadd31d8d106a2e6f3fd88
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-07-11 19:18:48 -0300

    update the if conditional adding the function trim() that clean white space at the begining and at the end of a string

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,38 +1,28 @@
-
-
-
 const list = document.getElementById("list");
 
 const input = document.getElementById("favchap");
 
 const btn = document.querySelector("button");
 
-
 btn.addEventListener("click", () => {
+  if (input.value.trim() !== "") {
+    const liBookName = document.createElement("li");
 
-    if (input.value != "")
-    {
-            const liBookName = document.createElement("li");
+    const deleteBtn = document.createElement("button");
 
-            const deleteBtn = document.createElement("button");
+    liBookName.textContent = input.value;
+    deleteBtn.textContent = "❌";
 
-            liBookName.textContent = input.value;
-            deleteBtn.textContent = "❌";
+    liBookName.append(deleteBtn);
+    list.append(liBookName);
 
-            liBookName.append(deleteBtn);
-            list.append(liBookName);
-            
-
-            deleteBtn.addEventListener("click", () => {
-                list.removeChild(liBookName);
-                input.focus();
-                input.value = "";
-            });
-            
-        }
-    else {
-        alert("You need to enter a book and Chapter!")
-        input.focus();
-    };
+    deleteBtn.addEventListener("click", () => {
+      list.removeChild(liBookName);
+      input.focus();
+      input.value = "";
+    });
+  } else {
+    alert("You need to enter a book and Chapter!");
+    input.focus();
+  }
 });
-
